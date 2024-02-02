7b80c321694002674bd6fe9d9461dd755b47e870
Thomas Rayner <thmsrynr@outlook.com>
2024-02-02 14:52:49 -0800

    less dumb solution

--- 01/main.go
+++ 01/main.go
@@ -21,34 +21,11 @@ func main() {
 
 	for _, s := range splitContent {
 		if s == "" {
-			// New line means new elf, evaluate the last elf
-			// Find where the new elf fits in, shuffle the others around if needed
-			if len(overallMax) > 2 {
-				if runningMax > overallMax[0] {
-					overallMax[2] = overallMax[1]
-					overallMax[1] = overallMax[0]
-					overallMax[0] = runningMax
-				} else if runningMax > overallMax[1] {
-					overallMax[2] = overallMax[1]
-					overallMax[1] = runningMax
-				} else if runningMax > overallMax[2] {
-					overallMax[2] = runningMax
-				}
-			} else {
-				// Not up to 3 elves yet, don't bother doing the comparison
-				overallMax = append(overallMax, runningMax)
-			}
-
-			// Reset and move on to the next elf
+			overallMax = append(overallMax, runningMax)
 			runningMax = 0
-			sort.Slice(overallMax, func(i, j int) bool {
-				return overallMax[i] > overallMax[j]
-			})
 			continue
 		}
 
-		// Figure out the number on the current line and append it to the total
-		// carried by the current elf
 		cur, err := strconv.Atoi(s)
 		if err != nil {
 			log.Fatal(err)
@@ -56,6 +33,10 @@ func main() {
 
 		runningMax += cur
 	}
+
+	sort.Slice(overallMax, func(i, j int) bool {
+		return overallMax[i] > overallMax[j]
+	})
 
 	fmt.Printf("P1: %d \n", overallMax[0])
 	fmt.Printf("P2: %d \n", overallMax[0]+overallMax[1]+overallMax[2])
